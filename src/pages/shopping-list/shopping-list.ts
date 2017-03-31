f32d71ae6148a9417ed54c98d4da0940ca802ef7
A.J. Satcher <a.j.satcher@AJs-MacBook-Pro.local>
2017-03-31 11:46:08 -0400

    edited submit

--- src/pages/shopping-list/shopping-list.ts
+++ src/pages/shopping-list/shopping-list.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
+import { NgForm } from "@angular/forms";
 @Component({
   selector: 'page-shopping-list',
   templateUrl: 'shopping-list.html'
 })
 export class ShoppingListPage {
+  onAddItem(form: NgForm){
+    console.log(form);
+  }
 }
