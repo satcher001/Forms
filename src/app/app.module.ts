437fcc45214a4a77c9bfa8bbf4bb4af8b884d41c
A.J. Satcher <a.j.satcher@AJs-MacBook-Pro.local>
2017-04-05 17:59:58 -0400

    edited

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,15 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-
+import { ShoppingListPage } from '../pages/shopping-list/shopping-list';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    ShoppingListPage
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -17,7 +18,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    ShoppingListPage
   ],
   providers: [
     StatusBar,
